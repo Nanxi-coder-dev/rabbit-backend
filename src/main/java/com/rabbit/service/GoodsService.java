7ef5d48765b964950c17fc9178c46cebf457e419
Nanxi-coder-dev <1944371908@qq.com>
2026-09-24 18:52:55 +0800

    feat: 添加商品详情Service组装逻辑

---
+++ src/main/java/com/rabbit/service/GoodsService.java
@@ -0,0 +1,18 @@
+package com.rabbit.service;
+
+import com.rabbit.vo.GoodsDetailVO;
+import com.rabbit.vo.GoodsSimpleVO;
+
+import java.util.List;
+
+public interface GoodsService {
+
+    /** 商品详情 */
+    GoodsDetailVO getDetail(Long id);
+
+    /** 热榜商品 */
+    List<GoodsSimpleVO> getHotGoods(Long id, Integer type, Integer limit);
+
+    /** 相关推荐 */
+    List<GoodsSimpleVO> getRelevant(Integer limit);
+}
