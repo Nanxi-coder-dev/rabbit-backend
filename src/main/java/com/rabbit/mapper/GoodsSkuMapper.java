77cb2498d412f981d693a746951fd6328309dade
Nanxi-coder-dev <1944371908@qq.com>
2026-09-25 17:57:46 +0800

    refactor: 删除重复的 Goods/GoodsSku/Category 实体与 Mapper

--- src/main/java/com/rabbit/mapper/GoodsSkuMapper.java
+++
@@ -1,9 +0,0 @@
-package com.rabbit.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.rabbit.entity.GoodsSku;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface GoodsSkuMapper extends BaseMapper<GoodsSku> {
-}
